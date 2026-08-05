5a61f68f3f3098aa9f01c9e1d828a7eedac1dac4
Yuvaraj Govindarajan <47356679+gyuv@users.noreply.github.com>
2026-08-05 19:25:55 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,23 +2,12 @@
 const nextConfig = {
   images: {
     remotePatterns: [
-      { protocol: "https", hostname: "image.tmdb.org" },
+      {
+        protocol: 'https',
+        hostname: 'image.tmdb.org',
+      },
     ],
   },
-};
-
-module.exports = nextConfig;
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['image.tmdb.org'], // Crucial for TMDB posters
-  },
-  // This helps if you're using getServerSideProps a lot
-  experimental: {
-    serverComponentsExternalPackages: ['react-i18next'], // If you use i18n
-  },
-  // Prevents infinite builds if you have a lot of dynamic pages
-  staticPageGenerationTimeout: 60,
 };
 
 module.exports = nextConfig;
