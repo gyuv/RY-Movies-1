696b54e3a55989c85451d70c5652373260646e61
Yuvaraj Govindarajan <47356679+gyuv@users.noreply.github.com>
2026-08-05 19:13:21 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -8,3 +8,17 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['image.tmdb.org'], // Crucial for TMDB posters
+  },
+  // This helps if you're using getServerSideProps a lot
+  experimental: {
+    serverComponentsExternalPackages: ['react-i18next'], // If you use i18n
+  },
+  // Prevents infinite builds if you have a lot of dynamic pages
+  staticPageGenerationTimeout: 60,
+};
+
+module.exports = nextConfig;
