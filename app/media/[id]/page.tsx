61b8c9981f2870d9f1a7b24b2dc1215b5acbf9c2
Yuvaraj Govindarajan <47356679+gyuv@users.noreply.github.com>
2026-08-06 20:52:47 +0530

    Update page.tsx

--- app/media/[id]/page.tsx
+++ app/media/[id]/page.tsx
@@ -1,3 +1,4 @@
+import StreamingPlayer from '../../components/StreamingPlayer';
 import MultiServerPlayer from '../../components/MultiServerPlayer';
 import StreamSelector from '../../components/StreamSelector';
 import Link from 'next/link';
