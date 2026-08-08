ef451e3ce5bfa1fb1fa11913b2bdead21b3e0185
Yuvaraj Govindarajan <47356679+gyuv@users.noreply.github.com>
2026-08-08 16:34:35 +0530

    Update page.tsx

--- app/movies/page.tsx
+++ app/movies/page.tsx
@@ -1,5 +1,6 @@
 import MoviesSection from '../components/MoviesSection';
 import Pagination from '../components/Pagination';
+import SectionFilters from '../components/SectionFilters';
 
 const MOCK_MOVIE = {
   id: 99, title: "Mock Movie", poster_path: "/9lH0V6e4b4w8r5k6j7h8g9f0d1s2a3.jpg",
